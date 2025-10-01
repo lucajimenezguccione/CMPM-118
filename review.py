d400e108eaa7e7b558afaa9e4e53af40d0a2bf9a
Luca Guccione-Jimenez <lguccion@ucsc.edu>
2025-10-01 10:25:12 -0700

    Wrote a review haiku

---
+++ review.py
@@ -0,0 +1,10 @@
+def PrintHaiku():
+	print("The Batman is cool.")
+	print("I love watching The Batman")
+	print("when it's October.")
+
+def main():
+	PrintHaiku()
+
+if __name__ == "__main__":
+	main()
